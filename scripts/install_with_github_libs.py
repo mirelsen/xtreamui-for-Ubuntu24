963ca8b4cc1c154e6f3ffd33a0d94362c9fbfede
mirelsen <mirelsen@github.com>
2025-06-07 07:59:34 +0000

    Cleaned repository - removed all files for fresh start

--- scripts/install_with_github_libs.py
+++
@@ -1,3 +0,0 @@
-# Această linie va fi înlocuită cu scriptul complet care folosește librariile din GitHub
-# TODO: Modifică scriptul să descarce librariile din repository-ul GitHub
-print("Script placeholder - va fi actualizat să folosească librariile din GitHub") 
